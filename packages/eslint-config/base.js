69c7140ec0a9a0dec974a7c5946e0b07ce06c0b4
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-09 12:57:07 +0200

    chore: Update eslint-config/base.js

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,23 +1,56 @@
 /** @type {import('@types/eslint').Linter.Config} */
 module.exports = {
   extends: [
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-    "prettier",
-    "plugin:import/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  rules: {
-    "import/no-duplicates": ["warn", { "prefer-inline": true }],
-    "import/consistent-type-specifier-style": ["warn", "prefer-inline"],
-    "import/order": [
-      "warn",
-      {
-        alphabetize: { order: "asc", caseInsensitive: true },
-        groups: [["builtin", "external"], "internal", "parent", "sibling"],
-        "newlines-between": "always",
-        pathGroups: [{ pattern: "~/**", group: "parent" }],
-      },
-    ],
+    '@vercel/style-guide/eslint/node',
+    '@vercel/style-guide/eslint/typescript',
+    'eslint-config-turbo'
+  ].map(require.resolve),
+  parser: '@typescript-eslint/parser',
+  env: {
+    node: true
   },
-};
+  rules: {
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+
+    'import/consistent-type-specifier-style': ['warn', 'prefer-inline'],
+    'import/no-duplicates': ['warn', { 'prefer-inline': true }],
+
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        disallowTypeAnnotations: true,
+        fixStyle: 'inline-type-imports'
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        caughtErrors: 'none',
+        destructuredArrayIgnorePattern: '^_*',
+        argsIgnorePattern: '^_*'
+      }
+    ],
+    'import/order': [
+      'error',
+      {
+        'alphabetize': {
+          order: 'asc',
+          caseInsensitive: true
+        },
+        'groups': [['builtin'], ['external'], 'internal', 'parent', 'sibling'],
+        'newlines-between': 'always',
+        'pathGroups': [
+          {
+            pattern: '~/**',
+            group: 'parent'
+          }
+        ]
+      }
+    ]
+  }
+}
